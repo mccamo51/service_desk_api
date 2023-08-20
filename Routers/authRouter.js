ab522e495ca37e9ba5da8ace8b8d85f1ad747670
mccamo51 <mccamo51@gmail.com>
2023-08-20 23:16:08 +0000

    added login endpoint

--- Routers/authRouter.js
+++ Routers/authRouter.js
@@ -4,6 +4,9 @@ const User = require("../Models/UserModel");
 const jwt = require("jsonwebtoken");
 const multer = require("multer");
 
+var emailRegex =
+  /^[-!#$%&'*+\/0-9=?A-Z^_a-z{|}~](\.?[-!#$%&'*+\/0-9=?A-Z^_a-z`{|}~])*@[a-zA-Z0-9](-*\.?[a-zA-Z0-9])*\.[a-zA-Z](-?[a-zA-Z0-9])+$/;
+var phoneno = /^\d{10}$/;
 const storage = multer.diskStorage({
   destination: (req, res, cb) => {
     cb(null, "public/uploads/");
@@ -21,7 +24,6 @@ authRouter.post(
   "/register",
   upload.single("profileImage"),
   async (req, res) => {
-    // console.log(req.body)
     const {
       phoneNumber,
       firstName,
@@ -31,11 +33,24 @@ authRouter.post(
       confirmPassword,
     } = req.body;
     const profileImage = req.file;
+
+    //Validating email using regEx
+    var valid = emailRegex.test(email);
+
     try {
       if (!phoneNumber)
         return res
           .status(400)
           .json({ errorMessage: "Phone number is required" });
+      if (phoneNumber.length < 10 || phoneNumber.length > 11)
+        return res
+          .status(400)
+          .json({ errorMessage: "Enter a valid phone number" });
+      if (!valid)
+        return res
+          .status(400)
+          .json({ errorMessage: "Enter a valid email address" });
+
       if (!firstName || !lastName)
         return res
           .status(400)
@@ -48,6 +63,7 @@ authRouter.post(
       if (confirmPassword !== password)
         return res.status(400).json({ errorMessage: "Password mismatch" });
 
+      //Checking if user exist
       const ifUserExist = await User.findOne({ phoneNumber: phoneNumber });
       const ifEmailExist = await User.findOne({ email: email });
       //Harshing Password using Salt Algorithm
@@ -64,44 +80,159 @@ authRouter.post(
         profilePic: profileImage.path,
       });
       const savedUser = await saveUser.save();
+      //Generating Cookies
       const token = jwt.sign(
         {
           user: saveUser.id,
         },
         process.env.TOKEN_SECRETE
       );
+
       //Setting Cookeis
-      res.cookie("access_token", token, {
-        httpOnly: true,
-      }).send();
+      res
+        .cookie("access_token", token, {
+          httpOnly: true,
+        })
 
       //Returning User Details
-      res.json({
-        status: true,
-        token: {
-          access_token: token,
-        },
-        data: {
-          email: savedUser.email,
-          firstName: savedUser.firstName,
-          lastName: savedUser.lastName,
-          profilePic: savedUser.profilePic,
-          phoneNumber:savedUser.phoneNumber
-        },
-      });
+        res.json({
+          status: true,
+          token: {
+            access_token: token,
+          },
+          data: {
+            email: savedUser.email,
+            firstName: savedUser.firstName,
+            lastName: savedUser.lastName,
+            profilePic: savedUser.profilePic,
+            phoneNumber:savedUser.phoneNumber
+          },
+        });
 
       // if(!profileImage) return res.status(400).json({errorMessage:"Profile Pic is required"})
     } catch (error) {
-        console.error(error)
+      console.error(error);
 
-        res.status(500).send();
+      res.status(500).send();
     }
   }
 );
 
-authRouter.post("/login", (req, res) => {
+authRouter.post("/login", async (req, res) => {
   try {
+    const { phoneNumber, email, password } = req.body;
+
+    var valid = emailRegex.test(email);
+    const phone = phoneno.test(phoneNumber);
+    if (email === undefined) {
+      if (!phoneNumber || !password)
+        return res
+          .status(400)
+          .json({ errorMessage: "All fields are required" });
+      if (!phone)
+        return res
+          .status(400)
+          .json({ errorMessage: "Enter a valid phone number" });
+
+      const ifUserExist = await User.findOne({ phoneNumber: phoneNumber });
+      const ifEmailExist = await User.findOne({ email: email });
+      //Harshing Password using Salt Algorithm
+      if (!ifUserExist)
+        return res
+          .status(400)
+          .json({ errorMessage: "Phone number or password incorrect" });
+
+      const passwordCorrect = await bcrypt.compare(
+        password,
+        ifUserExist.harshedPassword
+      );
+
+      if (passwordCorrect) {
+        const token = jwt.sign(
+          {
+            user: ifUserExist.id,
+          },
+          process.env.TOKEN_SECRETE
+        );
+        //Setting Cookies
+        res.cookie("access_token", token, {
+          httpOnly: true,
+        });
+        //Returning User Data
+        res.json({
+          status: 200,
+          userDetails: {
+            phone: ifUserExist.phoneNumber,
+          },
+        });
+      } else {
+        return res
+          .status(400)
+          .json({ errorMessage: "Email or password incorrect" });
+      }
+    } else {
+      if (!valid)
+        return res
+          .status(400)
+          .json({ errorMessage: "Enter a valid email address" });
+
+      const ifEmailExist = await User.findOne({ email: email });
+      //Harshing Password using Salt Algorithm
+
+      //Harshing Password using Salt Algorithm
+      if (!ifEmailExist)
+        return res
+          .status(400)
+          .json({ errorMessage: "Email or password incorrect" });
+
+      const passwordCorrect = await bcrypt.compare(
+        password,
+        ifEmailExist.harshedPassword
+      );
+
+      if (passwordCorrect) {
+        const token = jwt.sign(
+          {
+            user: ifEmailExist.id,
+          },
+          process.env.TOKEN_SECRETE
+        );
+        //Setting Cookies
+        res.cookie("access_token", token, {
+          httpOnly: true,
+        });
+        //Returning User Data
+        res.json({
+          status: 200,
+          userDetails: {
+            phone: ifEmailExist.phoneNumber,
+          },
+        });
+      } else {
+        return res
+          .status(400)
+          .json({ errorMessage: "Phone number or password incorrect" });
+      }
+    }
   } catch (error) {}
 });
+
+
+authRouter.put("/updateUser", (req, res)=>{
+    try {
+        const {
+            phoneNumber,
+            firstName,
+            lastName,
+            email,
+            password,
+            confirmPassword,
+          } = req.body;
+
+          
+    } catch (error) {
+        
+    }
+})
 
 module.exports = authRouter;
