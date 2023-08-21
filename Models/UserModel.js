3836fc821d34fe7bb24e5d0b99bdf947cb5ed338
mccamo51 <mccamo51@gmail.com>
2023-08-21 11:08:10 +0000

    added change password and update user profile

--- Models/UserModel.js
+++ Models/UserModel.js
@@ -1,6 +1,8 @@
 const mongoose = require("mongoose")
 
-
+var phoneno = /^\d{10}$/;
+var emailRegex =
+  /^[-!#$%&'*+\/0-9=?A-Z^_a-z{|}~](\.?[-!#$%&'*+\/0-9=?A-Z^_a-z`{|}~])*@[a-zA-Z0-9](-*\.?[a-zA-Z0-9])*\.[a-zA-Z](-?[a-zA-Z0-9])+$/;
 const userSchema = mongoose.Schema({
     firstName:{
         type:String,
@@ -12,7 +14,8 @@ const userSchema = mongoose.Schema({
     },
     phoneNumber:{
         type:String,
-        require
+        require,
+        match:[phoneno, "Enter a valid phone number"]
     },
     profilePic:{
         type:String,
@@ -23,6 +26,10 @@ const userSchema = mongoose.Schema({
     },
     email:{
         type:String,
+        match:[
+            emailRegex,
+            "Enter a valid email" 
+        ]
         
     },
 })
--- Routers/authRouter.js
+++ Routers/authRouter.js
@@ -34,22 +34,17 @@ authRouter.post(
     } = req.body;
     const profileImage = req.file;
 
+    console.log(email);
+    console.log(phoneNumber);
+
     //Validating email using regEx
-    var valid = emailRegex.test(email);
+    // var valid = emailRegex.test(email);
 
     try {
       if (!phoneNumber)
         return res
           .status(400)
           .json({ errorMessage: "Phone number is required" });
-      if (phoneNumber.length < 10 || phoneNumber.length > 11)
-        return res
-          .status(400)
-          .json({ errorMessage: "Enter a valid phone number" });
-      if (!valid)
-        return res
-          .status(400)
-          .json({ errorMessage: "Enter a valid email address" });
 
       if (!firstName || !lastName)
         return res
@@ -89,31 +84,39 @@ authRouter.post(
       );
 
       //Setting Cookeis
-      res
-        .cookie("access_token", token, {
-          httpOnly: true,
-        })
+      res.cookie("access_token", token, {
+        httpOnly: true,
+      });
 
       //Returning User Details
-        res.json({
-          status: true,
-          token: {
-            access_token: token,
-          },
-          data: {
-            email: savedUser.email,
-            firstName: savedUser.firstName,
-            lastName: savedUser.lastName,
-            profilePic: savedUser.profilePic,
-            phoneNumber:savedUser.phoneNumber
-          },
-        });
+      res.json({
+        status: true,
+        token: {
+          access_token: token,
+        },
+        data: {
+          email: savedUser.email,
+          firstName: savedUser.firstName,
+          lastName: savedUser.lastName,
+          profilePic: savedUser.profilePic,
+          phoneNumber: savedUser.phoneNumber,
+        },
+      });
 
       // if(!profileImage) return res.status(400).json({errorMessage:"Profile Pic is required"})
     } catch (error) {
-      console.error(error);
+      if (error.name === "ValidationError") {
+        const errorMessages = {};
 
-      res.status(500).send();
+        // Iterate through the validation errors and store custom error messages
+        for (let key in error.errors) {
+          errorMessages[key] = error.errors[key].message;
+        }
+
+        res.status(400).json({ errors: errorMessages });
+      } else {
+        res.status(500).json({ message: "An error occurred" });
+      }
     }
   }
 );
@@ -135,7 +138,6 @@ authRouter.post("/login", async (req, res) => {
           .json({ errorMessage: "Enter a valid phone number" });
 
       const ifUserExist = await User.findOne({ phoneNumber: phoneNumber });
-      const ifEmailExist = await User.findOne({ email: email });
       //Harshing Password using Salt Algorithm
       if (!ifUserExist)
         return res
@@ -214,25 +216,112 @@ authRouter.post("/login", async (req, res) => {
           .json({ errorMessage: "Phone number or password incorrect" });
       }
     }
+  } catch (error) {
+    console.log(error);
+    if (error.name === "ValidationError") {
+      const errorMessages = {};
+
+      // Iterate through the validation errors and store custom error messages
+      for (let key in error.errors) {
+        errorMessages[key] = error.errors[key].message;
+      }
+
+      res.status(400).json({ errors: errorMessages });
+    } else {
+      res.status(500).json({ message: "An error occurred" });
+    }
+  }
+});
+
+authRouter.put("/updateUser", async (req, res) => {
+  try {
+    const { phoneNumber, firstName, lastName, email } = req.body;
+    const { access_token } = req.cookies;
+    const userID = jwt.decode(access_token);
+
+    var valid = emailRegex.test(email);
+    const phone = phoneno.test(phoneNumber);
+
+    if (!phoneNumber || !firstName || !email || !lastName)
+      return res.status(400).json({ errorMessage: "All fields are required" });
+
+    if (!valid)
+      return res
+        .status(400)
+        .json({ errorMessage: "Enter a valid email address" });
+
+    if (!phone)
+      return res
+        .status(400)
+        .json({ errorMessage: "Enter a valid email address" });
+
+    const ifUserExist = await User.findById(userID.user);
+
+    if (!ifUserExist) {
+      return res.status(400).json({ errorMessage: "User does not exist" });
+    } else {
+      if (userID.user === ifUserExist.id) {
+        const updatedUser = await ifUserExist.updateOne({
+          firstName,
+          lastName,
+          email,
+          phoneNumber,
+        });
+        res.json({ message: 'User details updated successfully' });
+
+      } else {
+        console.log("updatedUser");
+
+        return res.status(401).json({ errorMessage: "User not authenticated" });
+      }
+    }
   } catch (error) {}
 });
 
+authRouter.put("/changePassword", async (req, res)=>{
+  try {
+    const { access_token } = req.cookies;
+    const userID = jwt.decode(access_token);
+    const {password, newPassword, confirmPassword}= req.body
 
-authRouter.put("/updateUser", (req, res)=>{
-    try {
-        const {
-            phoneNumber,
-            firstName,
-            lastName,
-            email,
-            password,
-            confirmPassword,
-          } = req.body;
+    const ifUserExist = await User.findById(userID.user);
+    const passwordCorrect = await bcrypt.compare(
+        password,
+        ifUserExist.harshedPassword
+      );
 
-          
-    } catch (error) {
+      if (!newPassword || !password || !newPassword)
+      return res
+        .status(400)
+        .json({ errorMessage: "All fields are required" });
+
+    if (passwordCorrect) {
         
-    }
+        if(newPassword !== confirmPassword) return res
+        .status(400)
+        .json({ errorMessage: "Password does not match" });
+        //Harsh new Password
+
+        const salt = await bcrypt.genSalt();
+      const harshedPassword = await bcrypt.hash(newPassword, salt);
+
+      ifUserExist.harshedPassword = harshedPassword;
+      await ifUserExist.save()
+       
+
+      res.json({ message: 'Password changed successfully' });
+        //Returning User Data
+       
+      } else {
+        return res
+          .status(400)
+          .json({ errorMessage: "Password incorrect" });
+      }
+  } catch (error) {
+    
+  }
 })
+
+
 
 module.exports = authRouter;
