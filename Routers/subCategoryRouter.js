2bcfcadcb638f0d3eec838c414d8e2068c6658ac
mccamo51 <mccamo51@gmail.com>
2023-08-27 19:31:55 +0000

    update

--- Routers/subCategoryRouter.js
+++ Routers/subCategoryRouter.js
@@ -17,24 +17,25 @@ subCategoryRouter.post("/addSubCategory", async (req, res) => {
       return res
         .status(400)
         .json({ errorMessage: "Subcategory already exist" });
-    await SubCategory({ category, name: title }).save(
-        { runValidators: true, context: 'query' },
-    );
+    await SubCategory({ category, name: title }).save({
+      runValidators: true,
+      context: "query",
+    });
 
     res.status(200).json({ message: "Sub Category added successfully" });
   } catch (error) {
     if (error.name === "ValidationError") {
-        const errorMessages = {};
-  
-        // Iterate through the validation errors and store custom error messages
-        for (let key in error.errors) {
-          errorMessages[key] = error.errors[key].message;
-        }
-  
-        res.status(400).json({ errors: errorMessages });
-      } else {
-        res.status(500).json({ message: "An error occurred" });
+      const errorMessages = {};
+
+      // Iterate through the validation errors and store custom error messages
+      for (let key in error.errors) {
+        errorMessages[key] = error.errors[key].message;
       }
+
+      res.status(400).json({ errors: errorMessages });
+    } else {
+      res.status(500).json({ message: "An error occurred" });
+    }
   }
 });
 
@@ -46,93 +47,89 @@ subCategoryRouter.put("/updateSubCategory", async (req, res) => {
 
     const subcategory = await SubCategory.findById(subCatId);
 
-    if(!subcategory) return res
-    .status(400)
-    .json({ errorMessage: "Subcategory does not exist" });
+    if (!subcategory)
+      return res
+        .status(400)
+        .json({ errorMessage: "Subcategory does not exist" });
 
-   
     subcategory.name = title;
 
-    await subcategory.save(
-        { runValidators: true, context: 'query' },
-    )
+    await subcategory.save({ runValidators: true, context: "query" });
     res.status(200).json({ message: "Sub Category update successfully" });
-
-
   } catch (error) {
     if (error.name === "ValidationError") {
-        const errorMessages = {};
-  
-        // Iterate through the validation errors and store custom error messages
-        for (let key in error.errors) {
-          errorMessages[key] = error.errors[key].message;
-        }
-  
-        res.status(400).json({ errors: errorMessages });
-      } else {
-        res.status(500).json({ message: "An error occurred" });
+      const errorMessages = {};
+
+      // Iterate through the validation errors and store custom error messages
+      for (let key in error.errors) {
+        errorMessages[key] = error.errors[key].message;
       }
+
+      res.status(400).json({ errors: errorMessages });
+    } else {
+      res.status(500).json({ message: "An error occurred" });
+    }
   }
 });
 
-subCategoryRouter.delete("/deleteSubCategory/:id", async (req, res)=>{
-    const category = (req.params.id)
-    try{
+subCategoryRouter.delete("/deleteSubCategory/:id", async (req, res) => {
+  const category = req.params.id;
+  try {
+    if (!category)
+      return res
+        .status(400)
+        .json({ errorMessage: "No post selected to be deleted" });
 
-    
-    if(!category) return res.status(400).json({errorMessage:"No post selected to be deleted"})
-    
-    const response = await SubCategory.findOneAndDelete({_id: category})
-    if(!response) return res.status(400).json({error:"Category does not exist"})
-        
-    res.status(200).json({message:"Category deleted successfully"})
-    } catch (error) {
-        if (error.name === "ValidationError") {
-            const errorMessages = {};
-      
-            // Iterate through the validation errors and store custom error messages
-            for (let key in error.errors) {
-              errorMessages[key] = error.errors[key].message;
-            }
-      
-            res.status(400).json({ errors: errorMessages });
-          } else {
-            res.status(500).json({ message: "An error occurred" });
-          }
+    const response = await SubCategory.findOneAndDelete({ _id: category });
+    if (!response)
+      return res.status(400).json({ error: "Category does not exist" });
+
+    res.status(200).json({ message: "Category deleted successfully" });
+  } catch (error) {
+    if (error.name === "ValidationError") {
+      const errorMessages = {};
+
+      // Iterate through the validation errors and store custom error messages
+      for (let key in error.errors) {
+        errorMessages[key] = error.errors[key].message;
+      }
+
+      res.status(400).json({ errors: errorMessages });
+    } else {
+      res.status(500).json({ message: "An error occurred" });
     }
-})
+  }
+});
 
-subCategoryRouter.get("/getSubCatById", async (req, res)=>{
-    const {subCatId} = req.query;
-    let subCategoryData;
-    try {
-        if(subCatId === undefined){
-            
-             subCategoryData =await SubCategory.find().populate()
-        }else{
-             subCategoryData =await SubCategory.find({name:subCatId}).populate()
-
-        }
-       console.log(subCategoryData,subCatId )
-    } catch (error) {
-        
+subCategoryRouter.get("/getSubCatById", async (req, res) => {
+  const { subCatId } = req.query;
+  let subCategoryData;
+  try {
+    if (subCatId === undefined) {
+      subCategoryData = await SubCategory.find().populate();
+    } else {
+      subCategoryData = await SubCategory.find({ name: subCatId }).populate();
     }
-})
+    res.status(200).json(subCategoryData);
 
-subCategoryRouter.get("/getAllSubCate", async (req, res)=>{
-    // const {subCatId} = req.query;
-    let subCategoryData;
-    try {
-        // if(subCatId === undefined){
-            
-             subCategoryData =await SubCategory.find().populate()
-        // }else{
-        //      subCategoryData =await SubCategory.find({name:subCatId}).populate()
+    // console.log(subCategoryData, subCatId);
+  } catch (error) {}
+});
 
-        // }
-       console.log(subCategoryData,subCatId )
-    } catch (error) {
-        
-    }
-})
+subCategoryRouter.get("/getAllSubCate", async (req, res) => {
+  // const {subCatId} = req.query;
+  let subCategoryData;
+  try {
+    // if(subCatId === undefined){
+
+    subCategoryData = await SubCategory.find().populate();
+    // }else{
+    //      subCategoryData =await SubCategory.find({name:subCatId}).populate()
+
+    // }
+    console.log(subCategoryData, subCatId);
+    res.status(200).json(subCategoryData);
+
+  } catch (error) {}
+});
 module.exports = subCategoryRouter;
